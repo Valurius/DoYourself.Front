11237a5642a3289f109c0af514a05c607f9c2102
Valurius <garimar525@gmail.com>
2024-03-05 16:16:12 +0300

    Menu2

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,31 +1,35 @@
-import { Sidebar, Menu, MenuItem, ProSidebarProvider} from "react-pro-sidebar";
-import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
-import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
-import ContactsOutlinedIcon from "@mui/icons-material/ContactsOutlined";
-import ReceiptOutlinedIcon from "@mui/icons-material/ReceiptOutlined";
-import CalendarTodayOutlinedIcon from "@mui/icons-material/CalendarTodayOutlined";
-import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
-import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
-
-function MenuBar() {
-
+import { Sidebar, SubMenu, Menu, MenuItem} from "react-pro-sidebar";
+import "../styles/componentStyles/Menu.css";
+import GridViewRoundedIcon from "@mui/icons-material/GridViewRounded";
+import ReceiptRoundedIcon from "@mui/icons-material/ReceiptRounded";
+import BarChartRoundedIcon from "@mui/icons-material/BarChartRounded";
+import TimelineRoundedIcon from "@mui/icons-material/TimelineRounded";
+import BubbleChartRoundedIcon from "@mui/icons-material/BubbleChartRounded";
+import WalletRoundedIcon from "@mui/icons-material/WalletRounded";
+import AccountBalanceRoundedIcon from "@mui/icons-material/AccountBalanceRounded";
+import SavingsRoundedIcon from "@mui/icons-material/SavingsRounded";
+import MonetizationOnRoundedIcon from "@mui/icons-material/MonetizationOnRounded";
+import SettingsApplicationsRoundedIcon from "@mui/icons-material/SettingsApplicationsRounded";
+import AccountCircleRoundedIcon from "@mui/icons-material/AccountCircleRounded";
+import ShieldRoundedIcon from "@mui/icons-material/ShieldRounded";
+import NotificationsRoundedIcon from "@mui/icons-material/NotificationsRounded";
+import LogoutRoundedIcon from "@mui/icons-material/LogoutRounded";
+const MenuBar = () =>{
   return (
     <div style={{ display: "flex", height: "100vh" }}>
-      <Sidebar className="sidebar">
-        <Menu>
-          <MenuItem className="menu1">
-            <h2>QUICKPAY</h2>
-          </MenuItem>
-          <MenuItem> Dashboard </MenuItem>
-          <MenuItem> Invoices </MenuItem>
-          <MenuItem> Charts </MenuItem>
-          <MenuItem> Wallets </MenuItem>
-          <MenuItem> Transactions </MenuItem>
-          <MenuItem> Settings </MenuItem>
-          <MenuItem> Logout </MenuItem>
-        </Menu>
-      </Sidebar>
-    </div>
+    <Sidebar className="sidebar">
+      <Menu className="menu" >
+      <SubMenu label="Задачи" icon={<BarChartRoundedIcon />}>
+          <MenuItem className="menu-item" icon={<TimelineRoundedIcon />}> Мои задачи </MenuItem>
+          <MenuItem className="menu-item" icon={<BubbleChartRoundedIcon />}> Все задачи</MenuItem>
+        </SubMenu>
+        <MenuItem icon={<GridViewRoundedIcon />}> Статистика </MenuItem>
+        <MenuItem icon={<ReceiptRoundedIcon />}> Участники </MenuItem>
+        
+      
+      </Menu>
+    </Sidebar>
+  </div>
   );
 }
 export default MenuBar;
